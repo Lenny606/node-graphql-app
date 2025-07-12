2165f575492dbaf9573c7a41ad371329c1bcae43
--help <you@example.com>
2025-07-12 17:42:05 +0200

    Add createUser resolver to handle user creation; include email extraction and placeholder response

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,25 @@
+//import ORM schema
+
 const resolvers = {
     hello() {
         return {
             text: 'Hello World',
             views: 1001
         };
+    },
+    createUser: async function ({userInput}, req) {
+        // const email = args.userInput.email
+        const email = userInput.email
+
+        //ORM LOGIC with USER
+
+
+        //response
+        return {
+            email: "test@email.cz"
+        }
+
     }
 };
 
-module.exports = { resolvers };
+module.exports = {resolvers};
